a9cc345ba1f02b973a4c23737021eb1af8ade310
Thiago099 <tecnologiasthiago@gmail.com>
2022-04-23 19:41:01 -0300

    [style] cleanup

--- src/components/named_entity/named_entity.vue
+++ src/components/named_entity/named_entity.vue
@@ -24,7 +24,7 @@ update_list()
     <h2 class="text-center">{{ title }}</h2>
     <div class="row">
       <div class="form-group" v-for="correlation in display_named_entity" :key="correlation">
-        <label for="name">Name:</label>
+        <!-- <label for="name">Name:</label> -->
         <div class="input-group">
           <input class="form-control" id="name" v-model="correlation.name">
           <button class="btn btn-danger" @click="del(correlation)"> <i class="fa fa-trash"></i></button>
