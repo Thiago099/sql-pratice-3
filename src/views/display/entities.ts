71498179f168fb8c7d04f98db4182613101afdf7
Thiago099 <thiago.kaique01@gmail.com>
2022-04-25 09:33:55 -0300

    [feature] example

--- src/views/display/entities.ts
+++ src/views/display/entities.ts
@@ -5,7 +5,12 @@ import { bake_entity, point_entity } from './entity_graphics'
 export const entities = ref<entity[]>([
     {
         position: { x: 100, y: 50 },
-        text: 'Hello World',
+        text: 'Hello world',
         image: bake_entity("Hello world")
+    },
+    {
+        position: { x: 400, y: 50 },
+        text: 'Hello world 1',
+        image: bake_entity("Hello world 2")
     }
 ])
